1a7b9b6f02b3297bb0b61f85047c65a8f968f057
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-02-22 13:13:11 +0600

    second commit

--- index.js
+++ index.js
@@ -10,31 +10,33 @@ app.use(express.json());
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${user}:${password}@cluster0.5e5ivqa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri);
-async function run() {
+// https://github.com/mdismail645221/ShopEase-assessment-sever/blob/main/index.js
+const users = client.db("UsersInfo").collection("users");
+const usersAdditionalInfo = client
+  .db("UsersInfo")
+  .collection("usersAdditionalInfo");
+const run = async () => {
   try {
-    const users = client.db("UsersInfo").collection("users");
-    const usersAdditionalInfo = client
-      .db("UsersInfo")
-      .collection("usersAdditionalInfo");
-    //   https://github.com/mdismail645221/ShopEase-assessment-sever
-    //   app.get("/", async (req, res) => {
-    //   })
-    //   app.get("/", async (req, res) => {
-    //   })
-    //   app.get("/", async (req, res) => {
-    //   })
-    //   app.get("/", async (req, res) => {
-    //   })
-    //   app.get("/", async (req, res) => {
-    //   })
-    //   app.get("/", async (req, res) => {
-    //   })
-  } finally {
+    await client.connect((err) => {
+      if (err) {
+        console.log(`${err}`.red);
+      } else {
+        console.log("no error".bgBlue);
+      }
+    });
+  } catch {
+    (error) => {
+      console.log(error);
+    };
   }
-}
-run().catch((error) => {
-  res.send({});
+};
+run();
+
+//  all api create below:
+app.get("/", async (req, res) => {
+  res.send({ message: "server is running" });
 });
 
 app.listen(port, () => {
