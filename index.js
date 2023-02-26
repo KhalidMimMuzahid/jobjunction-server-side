2e47f6f6b6681f0404275f8e716d4c045f677aed
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-02-26 18:31:56 +0600

    some-changes-done

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ app.use(express.json());
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${user}:${password}@cluster0.5e5ivqa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri);
 // https://github.com/mdismail645221/ShopEase-assessment-sever/blob/main/index.js
 const users = client.db("UsersInfo").collection("users");
-const usersAdditionalInfo = client
-  .db("UsersInfo")
-  .collection("usersAdditionalInfo");
+
+// collection
+const usersAdditionalInfo = client.db("UsersInfo").collection("usersAdditionalInfo");
+const timeLinePostsCollection = client.db("posts").collection("timeLinePosts")
+const jobPostsCollection = client.db("alljobposts").collection("jobPosts")
+
+
 const run = async () => {
   try {
     await client.connect((err) => {
@@ -39,18 +43,147 @@ app.get("/", async (req, res) => {
   res.send({ message: "server is running" });
 });
 
-app.post("/insertusertodb", async (req, res) => {
+
+// single post for timeline POST API
+app.post('/usersinglepost', async (req, res) => {
   try {
-    const userInfo = req.body;
-    console.log(userInfo);
-    const result = await users.insertOne(userInfo);
-    res.send(result);
-  } catch {
-    (error) => {
-      console.log(error);
-    };
+    const postData = req.body
+    // console.log(body)
+    const result = await timeLinePostsCollection.insertOne(postData)
+    if(result.acknowledged){
+      res.send({
+        success: true,
+        message: "Successfully got the data",
+        data: result,
+      })
+    }
+    else{
+      res.send({
+        success: false,
+        error: error.message,
+      })
+    }
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
   }
 });
-app.listen(port, () => {
-  console.log("listening on port", port);
+
+
+// all post GET API
+app.get('/allposts', async(req, res) => {
+  // try block
+  try {
+    const query = {}
+    const option = await timeLinePostsCollection.find(query).toArray()
+    res.send({
+      success: true,
+      message: "Successfully got the data",
+      data: option,
+    })
+  } 
+
+  // catch block
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+})
+
+
+
+// job POST API
+app.post('/postajob', async (req, res) => {
+  try {
+    const postData = req.body
+    // console.log(body)
+    const result = await jobPostsCollection.insertOne(postData)
+    if(result.acknowledged){
+      res.send({
+        success: true,
+        message: "Successfully got the data",
+        data: result,
+      })
+    }
+    else{
+      res.send({
+        success: false,
+        error: error.message,
+      }); 
+    }
+  }
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
+
+
+// all job post GET API
+app.get('/getalljobs', async (req, res) => {
+  // try block
+  try {
+    const query = {}
+    const option = await jobPostsCollection.find(query).toArray()
+    
+    if(option.acknowledged){
+      res.send({
+        success: true,
+        message: "Successfully got the data",
+        data: option,
+      })
+    }
+
+    else{
+      console.log(error.name.bgRed, error.message.bold);
+      res.send({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+
+  // catch block
+  catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+})
+
+
+
+
+
+
+    // user's data save POST API
+    app.post("/insertusertodb", async (req, res) => {
+      try {
+        const userInfo = req.body;
+        console.log(userInfo);
+        const result = await users.insertOne(userInfo);
+        res.send(result);
+      } catch {
+        (error) => {
+          console.log(error);
+        };
+      }
+    });
+
+
+
+    app.listen(port, () => {
+      console.log("listening on port", port);
+    });
