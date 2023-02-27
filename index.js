6defe202b2f66790116d718aa1ff480082ee5dcf
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-02-27 12:50:29 +0600

    updated

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ const client = new MongoClient(uri);
 const users = client.db("UsersInfo").collection("users");
 
 // collection
-const usersAdditionalInfo = client.db("UsersInfo").collection("usersAdditionalInfo");
-const timeLinePostsCollection = client.db("posts").collection("timeLinePosts")
-const jobPostsCollection = client.db("alljobposts").collection("jobPosts")
-
+const usersAdditionalInfo = client
+  .db("UsersInfo")
+  .collection("usersAdditionalInfo");
+const timeLinePostsCollection = client.db("posts").collection("timeLinePosts");
+const jobPostsCollection = client.db("alljobposts").collection("jobPosts");
 
 const run = async () => {
   try {
@@ -43,28 +44,25 @@ app.get("/", async (req, res) => {
   res.send({ message: "server is running" });
 });
 
-
 // single post for timeline POST API
-app.post('/usersinglepost', async (req, res) => {
+app.post("/usersinglepost", async (req, res) => {
   try {
-    const postData = req.body
+    const postData = req.body;
     // console.log(body)
-    const result = await timeLinePostsCollection.insertOne(postData)
-    if(result.acknowledged){
+    const result = await timeLinePostsCollection.insertOne(postData);
+    if (result.acknowledged) {
       res.send({
         success: true,
         message: "Successfully got the data",
         data: result,
-      })
-    }
-    else{
+      });
+    } else {
       res.send({
         success: false,
         error: error.message,
-      })
+      });
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error.name.bgRed, error.message.bold);
     res.send({
       success: false,
@@ -73,53 +71,19 @@ app.post('/usersinglepost', async (req, res) => {
   }
 });
 
-
 // all post GET API
-app.get('/allposts', async(req, res) => {
+app.get("/allposts", async (req, res) => {
   // try block
   try {
-    const query = {}
-    const option = await timeLinePostsCollection.find(query).toArray()
+    const query = {};
+    const option = await timeLinePostsCollection.find(query).toArray();
     res.send({
       success: true,
       message: "Successfully got the data",
       data: option,
-    })
-  } 
-
-  // catch block
-  catch (error) {
-    console.log(error.name.bgRed, error.message.bold);
-    res.send({
-      success: false,
-      error: error.message,
     });
-  }
-})
-
-
-
-// job POST API
-app.post('/postajob', async (req, res) => {
-  try {
-    const postData = req.body
-    // console.log(body)
-    const result = await jobPostsCollection.insertOne(postData)
-    if(result.acknowledged){
-      res.send({
-        success: true,
-        message: "Successfully got the data",
-        data: result,
-      })
-    }
-    else{
-      res.send({
-        success: false,
-        error: error.message,
-      }); 
-    }
-  }
-  catch (error) {
+  } catch (error) {
+    // catch block
     console.log(error.name.bgRed, error.message.bold);
     res.send({
       success: false,
@@ -128,62 +92,76 @@ app.post('/postajob', async (req, res) => {
   }
 });
 
+// job POST API
+app.post("/postajob", async (req, res) => {
+  try {
+    const postData = req.body;
+    // console.log(body)
+    const result = await jobPostsCollection.insertOne(postData);
+    if (result.acknowledged) {
+      res.send({
+        success: true,
+        message: "Successfully got the data",
+        data: result,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: error.message,
+      });
+    }
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // all job post GET API
-app.get('/getalljobs', async (req, res) => {
+app.get("/getalljobs", async (req, res) => {
   // try block
   try {
-    const query = {}
-    const option = await jobPostsCollection.find(query).toArray()
-    
-    if(option.acknowledged){
+    const query = {};
+    const option = await jobPostsCollection.find(query).toArray();
+
+    if (option.acknowledged) {
       res.send({
         success: true,
         message: "Successfully got the data",
         data: option,
-      })
-    }
-
-    else{
+      });
+    } else {
       console.log(error.name.bgRed, error.message.bold);
       res.send({
         success: false,
         error: error.message,
       });
     }
-  }
-
-  // catch block
-  catch (error) {
+  } catch (error) {
+    // catch block
     console.log(error.name.bgRed, error.message.bold);
     res.send({
       success: false,
       error: error.message,
     });
   }
-})
+});
+// user's data save POST API
+app.post("/insertusertodb", async (req, res) => {
+  try {
+    const userInfo = req.body;
+    console.log(userInfo);
+    const result = await users.insertOne(userInfo);
+    res.send(result);
+  } catch {
+    (error) => {
+      console.log(error);
+    };
+  }
+});
 
-
-
-
-
-
-    // user's data save POST API
-    app.post("/insertusertodb", async (req, res) => {
-      try {
-        const userInfo = req.body;
-        console.log(userInfo);
-        const result = await users.insertOne(userInfo);
-        res.send(result);
-      } catch {
-        (error) => {
-          console.log(error);
-        };
-      }
-    });
-
-
-
-    app.listen(port, () => {
-      console.log("listening on port", port);
-    });
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
