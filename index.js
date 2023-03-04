2480752df037a9ef405aea086a8ff2eb327984fd
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-03-05 03:15:57 +0600

    ok

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(port, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://job-junction-pro.web.app",
   },
 });
 io.on("connection", (socket) => {
@@ -664,34 +664,30 @@ app.post("/sendMessage", async (req, res) => {
   res.send({ sdfs: "fgfd" });
 });
 
-
-
 // apply a job PUT API
-app.put('/applyajob', async (req, res) => {
+app.put("/applyajob", async (req, res) => {
   try {
-    const infoNeedParse = req.headers.info
-    const info = JSON.parse(infoNeedParse)
-    const { _id, email } = info
+    const infoNeedParse = req.headers.info;
+    const info = JSON.parse(infoNeedParse);
+    const { _id, email } = info;
 
     // console.log(_id, email)
     // console.log(info)
 
-    const query = { _id: new ObjectId(_id) }
-    const option = await jobPostsCollection.updateOne(
-      query,
-      { $push: { totalApplyed: email } })
+    const query = { _id: new ObjectId(_id) };
+    const option = await jobPostsCollection.updateOne(query, {
+      $push: { totalApplyed: email },
+    });
 
     res.send({
       success: true,
       message: "Successfully got the data",
       data: option,
     });
-
-
   } catch (error) {
     res.send({
       success: false,
       error: error.message,
     });
   }
-})
+});
